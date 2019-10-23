ee31d2a4161f5fb56d94de2bc9f479bbc476f763
Ted Ogmundson <togmund@gmail.com>
2019-10-23 01:45:05 +0000

    initial commit of survey.js with challenge statement and boilerplate

---
+++ survey.js
@@ -0,0 +1,28 @@
+
+/*
+Instead of asking the user about what they think of Node.js, we can ask them the following questions, in order:
+
+What's your name? Nicknames are also acceptable :)
+What's an activity you like doing?
+What do you listen to while doing that?
+Which meal is your favourite (eg: dinner, brunch, etc.)
+What's your favourite thing to eat for that meal?
+Which sport is your absolute favourite?
+What is your superpower? In a few words, tell us what you are amazing at!
+That said, feel free to change the narrative if you have better, more interesting questions to ask them.
+
+Once all questions are answered, our survey app should output a fully formed paragraph for their online profile, similar to the one shown previously. It should then exit. The user would have to rerun the app to go through the process of generating another profile.
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
