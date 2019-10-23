1dc3e7aa1362749583d767bf74addf1465c292d9
Ted Ogmundson <togmund@gmail.com>
2019-10-23 02:22:23 +0000

    solved survey recursively, not sure if it works yet, still testing things

--- survey.js
+++ survey.js
@@ -1,17 +1,25 @@
 
 /*
+
+Focus on the following:
+https://github.com/nodejs/node/blob/master/doc/api/readline.md
+
+- The example usage shown at the beginning
+- The .question(query, callback) function
+- The .close() function
+
 Instead of asking the user about what they think of Node.js, we can ask them the following questions, in order:
 
-What's your name? Nicknames are also acceptable :)
-What's an activity you like doing?
-What do you listen to while doing that?
-Which meal is your favourite (eg: dinner, brunch, etc.)
-What's your favourite thing to eat for that meal?
-Which sport is your absolute favourite?
-What is your superpower? In a few words, tell us what you are amazing at!
-That said, feel free to change the narrative if you have better, more interesting questions to ask them.
+- What's your name? Nicknames are also acceptable :)
+- What's an activity you like doing?
+- What do you listen to while doing that?
+- Which meal is your favourite (eg: dinner, brunch, etc.)
+- What's your favourite thing to eat for that meal?
+- Which sport is your absolute favourite?
+- What is your superpower? In a few words, tell us what you are amazing at!
+- That said, feel free to change the narrative if you have better, more interesting questions to ask them.
 
-Once all questions are answered, our survey app should output a fully formed paragraph for their online profile, similar to the one shown previously. It should then exit. The user would have to rerun the app to go through the process of generating another profile.
+- Once all questions are answered, our survey app should output a fully formed paragraph for their online profile, similar to the one shown previously. It should then exit. The user would have to rerun the app to go through the process of generating another profile.
 */
 
 const readline = require('readline');
@@ -21,8 +29,29 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const questionnaire = 
+  [ `What's your preferred first name?\n`
+  , `What's an activity you like doing?\n`
+  , `What do you listen to while doing that?\n`
+  , `Which meal is your favourite (eg: dinner, brunch, etc.)\n`
+  , `What's your favourite thing to eat for that meal?\n`
+  , `Which sport is your absolute favourite?\n`
+  , `What is your superpower? In a few words, tell us what you are amazing at!\n`
+  ]
 
-  rl.close();
-});
+const questionAsker = (questions) => {
+  const answers = [];
+  for (const question of questions) {
+    rl.question(question, (answer) => {
+      if (questions === 1) {
+        answers.push(answer)
+        console.log(answers);
+        return rl.close();
+      }
+      answers.push(answer);
+      questionAsker(questions.slice(1));
+    }); 
+  }
+}
+
+questionAsker(questionnaire);
